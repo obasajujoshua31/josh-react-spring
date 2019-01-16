4b17607921b94c10301490dd3519aa664f0452e5
Joshua Obasaju <joshuaobasaju@Joshuas-MacBook-Pro.local>
2019-01-16 08:27:59 +0100

    Updates

---
+++ precache-manifest.05f6afbeef672b71775711c5128af9c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f526229813fc771db7c",
+    "url": "/josh-react-spring/static/js/runtime~main.6f526229.js"
+  },
+  {
+    "revision": "d9873bea9de0ae3f901d",
+    "url": "/josh-react-spring/static/js/main.d9873bea.chunk.js"
+  },
+  {
+    "revision": "234ceb3c1d50c4bf901c",
+    "url": "/josh-react-spring/static/js/1.234ceb3c.chunk.js"
+  },
+  {
+    "revision": "d9873bea9de0ae3f901d",
+    "url": "/josh-react-spring/static/css/main.e3bdc738.chunk.css"
+  },
+  {
+    "revision": "f675ab42b661536f0f872fa428bbf51e",
+    "url": "/josh-react-spring/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/josh-react-spring/precache-manifest.05f6afbeef672b71775711c5128af9c9.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/josh-react-spring/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
